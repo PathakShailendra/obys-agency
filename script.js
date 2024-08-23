cb3e7e80261d49fb70fd0eb0bf607a206fe55d8b
Shailendra pathak <pathakharsh813@gmail.com>
2024-08-23 11:05:21 +0530

    made loader successfully

---
+++ script.js
@@ -0,0 +1,41 @@
+var tl = gsap.timeline();
+tl.from(".line h1",{
+    y:150,
+    stagger:0.2,
+    duration:0.6,
+    delay:0.5
+})
+tl.from("#line1-part1",{
+    opacity:0,
+    onStart:function(){
+        var h5Timer = document.querySelector("#line1-part1 h5");
+        var grow = 0;
+        setInterval(function(){
+            if(grow < 100){
+                h5Timer.innerHTML = grow++;
+            }
+            else{
+                h5Timer.innerHTML = grow
+            }
+        },32)
+    }
+})
+tl.to(".line h2",{
+    animationName:"anime",
+    opacity:1
+})
+tl.to("#loader",{
+    opacity:0,
+    duration:0.4,
+    delay:4
+})
+tl.from("#page1",{
+    y:1200,
+    opacity:0,
+    duration:0.5,
+    delay:0.2,
+    ease:Power4
+})
+tl.to("#loader",{
+    display:"none"
+})
