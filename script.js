c35b4a494396c82873df73701a95780e7b550589
Shailendra pathak <pathakharsh813@gmail.com>
2024-08-25 16:02:53 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline();
+function loadingAnimation(){
+    var tl = gsap.timeline();
 tl.from(".line h1",{
     y:150,
     stagger:0.2,
@@ -27,7 +28,7 @@ tl.to(".line h2",{
 tl.to("#loader",{
     opacity:0,
     duration:0.4,
-    delay:4
+    delay:3
 })
 tl.from("#page1",{
     y:1200,
@@ -39,3 +40,28 @@ tl.from("#page1",{
 tl.to("#loader",{
     display:"none"
 })
+tl.from("#nav",{
+    opacity:0
+})
+tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
+    y:120,
+    stagger:0.2,
+    delay:-0.3
+})
+}
+function currentAnimation() {
+    // Cursor animation
+
+document.addEventListener("mousemove",function(dets) {
+    gsap.to("#crsr",{
+        left:dets.x,
+        top:dets.y,
+    })
+})
+
+Shery.makeMagnet("#nav-part2 h4");
+}
+
+
+loadingAnimation();
+currentAnimation();
